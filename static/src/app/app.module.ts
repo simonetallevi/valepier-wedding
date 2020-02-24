96d7b162a302b293a266722e7d736bc8977e53d3
Simone Tallevi-Diotallevi <simone.tallevi@pirelli.com>
2020-02-24 08:41:14 +0100

    start with header and body

--- static/src/app/app.module.ts
+++ static/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HeaderComponent} from "./header/header.component";
+import {BodyComponent} from "./body/body.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule,
