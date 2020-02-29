84bf8449150e63a886a83346059430fc9af79045
Simone Tallevi-Diotallevi <simone.tallevi@pirelli.com>
2020-02-29 23:38:35 +0100

    initial style

--- static/src/app/app.module.ts
+++ static/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import {HeaderComponent} from "./header/header.component";
 import {BodyComponent} from "./body/body.component";
 import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BodyComponent
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
